81c3d1e1586ca886e637bdbecdc787df698864ce
vitorbandeira1 <49555280+vitorbandeira1@users.noreply.github.com>
2019-04-16 16:09:44 -0300

    Add files via upload

---
+++ sem título0.py
@@ -0,0 +1,13 @@
+def main():
+    print('Correndo da DP')
+    print("--------------")
+    print("'Para o final do mês? Ahh suave, depois eu faço...' foi oque _____,"
+          "aluno de engenharia julgou como uma boa ideia quando o professor Raul"
+          "comentou sobre o EP, mas um fato raro aconteceu, o dom da procrastinação"
+          "abençoou o pobre ____...")
+    print()
+    print("É o dia da entrega, e como de costume o desespero bate forte, em seu trabalho"
+          "não havia um print. _____ como medida desesperada resolve ir voando para o Insper"
+          "tentar o impossivel: Fazer Raul adiar a entrega do trabalho!")
+    print()
+    
